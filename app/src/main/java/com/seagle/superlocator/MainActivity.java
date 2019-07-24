8f48fb2500d3d3983cea0c0b4d90ef574ef61630
jinshiyi11 <49662990@qq.com>
2019-07-24 23:51:34 +0800

    1.添加地图sdk

--- app/src/main/java/com/seagle/superlocator/MainActivity.java
+++ app/src/main/java/com/seagle/superlocator/MainActivity.java
@@ -1,13 +1,25 @@
 package com.seagle.superlocator;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
 
 public class MainActivity extends AppCompatActivity {
+    private MapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMapView = findViewById(R.id.mv_map);
+        mMapView.onCreate(savedInstanceState);
+        AMap aMap = mMapView.getMap();
+//        aMap.setTrafficEnabled(true);// 显示实时交通状况
+//        //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
+//        aMap.setMapType(AMap.MAP_TYPE_SATELLITE);// 卫星地图模式
     }
 }
